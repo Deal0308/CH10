35eb534ae5ac85512a80e5be00de28856643dc94
Michael Deal <Deal0308@yahoo.com>
2026-07-09 19:10:36 -0500

    Initial Commit

---
+++ CH10/CH10App.swift
@@ -0,0 +1,17 @@
+//
+//  CH10App.swift
+//  CH10
+//
+//  Created by Michael Deal on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CH10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
